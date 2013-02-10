3375a0ca53cc6b636b3f7d35e84583df4b014d4e
demanu <emmanuel.verbraecken@gmail.com>
2013-02-10 15:56:47 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oefening::Application.config.secret_token = '50cd297e514a911bfe1b4502d13b9ba4f4b6f05f428c27c84079be30b70c4a1b05dae863b20c7e9804063fea026c1fb358fd8c407e4c0e719ba2ca54b106651f'
